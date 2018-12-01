f8a26d19a3b9a14dab31e402145004d781bfa393
Lwooles <lisa.wooles@hotmail.co.uk>
2018-12-02 10:45:44 +1300

    add rectangle

--- Pong1/src/PongPanel.java
+++ Pong1/src/PongPanel.java
@@ -4,17 +4,32 @@
    import java.awt.event.KeyEvent;
    import java.awt.event.KeyListener;
    import javax.swing.JPanel;
-   
+   import javax.swing.Timer;
+   import java.awt.Graphics;
+   import java.awt.Graphics2D;
 
    
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	   
 	  private final static Color BACKGROUND_COLOUR = Color.BLACK;
-       
-      public PongPanel() {
-          setBackground(BACKGROUND_COLOUR);
-      }
+	  private final static int TIMER_DELAY = 5;
+      
+	  public PongPanel() {
+	      setBackground(BACKGROUND_COLOUR);
+	      Timer timer = new Timer(TIMER_DELAY, this);
+	         timer.start();
+	 }
+	  
+	  private void update() {		  
+	  }
   
+	  @Override
+	  public void paintComponent(Graphics g) {
+	      super.paintComponent(g);
+	      g.setColor(Color.WHITE);
+	      g.fillRect(20, 20, 100, 100);
+	  }
+	  
       @Override
       public void keyPressed(KeyEvent event) {
       }
@@ -29,6 +44,7 @@
   
       @Override
       public void actionPerformed(ActionEvent event) {
+    	  update();
       }
   
  }
